7dba63644d6444335e242f4dbd1154c22a95a0ff
ridi <ridi@mic.ms>
2021-12-03 06:29:24 +0700

    Chapter 1

---
+++ Chapter1/Modul1DasarCSharp/Car.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+namespace Modul1DasarCSharp
+{
+    public class Car
+    {
+         public string model;
+         public string brand;
+         public int enginesize;
+
+        
+    }
+}
---
+++ Chapter1/Modul1DasarCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Modul1DasarCSharp;
+
+Console.WriteLine("Hello, World!");
+
+Car mobilSaya = new Car();
+mobilSaya.brand = "Mazda";
+mobilSaya.model ="RX8";
+mobilSaya.enginesize = 2000;
+
+Console.WriteLine(mobilSaya.brand + mobilSaya.model + mobilSaya.enginesize.ToString());
+
