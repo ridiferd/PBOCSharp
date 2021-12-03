1123e4f8f91b66855ff2e87431178ff128d006ea
ridi <ridi@mic.ms>
2021-12-04 06:42:45 +0700

    Ditambahkan untuk pengenalan metode, parameter, dan library

--- Chapter1/Modul1DasarCSharp/Program.cs
+++ Chapter1/Modul1DasarCSharp/Program.cs
@@ -8,5 +8,17 @@ mobilSaya.brand = "Mazda";
 mobilSaya.model ="RX8";
 mobilSaya.enginesize = 2000;
 
+Car mobilAnda = new Car("Toyota","FT86", 2000);
+
+mobilAnda.StartEngine();
+
+int hasil = mobilAnda.CurrentSpeed(70);
+Console.WriteLine(hasil);
+
 Console.WriteLine(mobilSaya.brand + mobilSaya.model + mobilSaya.enginesize.ToString());
+
+Car.DisplayCar();
+
+double avg = mobilAnda.AverageSpeed(40,50,60,80,70,80,100);
+Console.WriteLine(avg);
 
