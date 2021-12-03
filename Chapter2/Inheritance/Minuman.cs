e835663846724cbd8cbc75e593429f97e121739b
Novrian <novrian@mic.ms>
2021-12-03 16:43:12 +0700

    add inheritance

---
+++ Chapter2/Inheritance/Minuman.cs
@@ -0,0 +1,9 @@
+public class Minuman {
+    public string Nama;
+    public string Rasa;
+    public int Harga;
+}
+
+public class Cafetaria : Minuman {
+    
+}
---
+++ Chapter2/Inheritance/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Minuman minum = new Minuman();
+minum.Nama = "Es Teh";
+minum.Rasa = "Manis";
+minum.Harga = 200;
