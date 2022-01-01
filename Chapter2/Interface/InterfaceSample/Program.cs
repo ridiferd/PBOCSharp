cfbe608e80be65ed8cbe55db87264936e8d6b356
ridi <ridi@mic.ms>
2022-01-01 17:22:02 +0700

    add interface chapter

---
+++ Chapter2/Interface/InterfaceSample/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+InterfaceSample.Rectangle a = new InterfaceSample.Rectangle();
+double hasil = a.CalculateArea(12,25);
+a.GetColor();
+
+InterfaceSample.IPolygon b = new InterfaceSample.Rectangle();
+hasil = b.CalculateArea(23,12);
