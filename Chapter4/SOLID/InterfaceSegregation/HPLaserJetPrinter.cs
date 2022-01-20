cd353a1007ca62dbac4dfec16aa67ce865bc405b
Novrian Fajar Hidayat <novrian@mic.ms>
2022-01-20 12:56:51 +0700

    Interface Segregation

---
+++ Chapter4/SOLID/InterfaceSegregation/HPLaserJetPrinter.cs
@@ -0,0 +1,47 @@
+/// Without Interface Segregation Principle
+public class HPLaserJetPrinter : IPrinterTasks
+{
+    public void Print(string PrintContent)
+    {
+        Console.WriteLine("Print Done");
+    }
+
+    public void Scan(string ScanContent)
+    {
+        Console.WriteLine("Scan content");
+    }
+
+    public void Fax(string FaxContent)
+    {
+        Console.WriteLine("Fax Content");
+    }
+
+    public void PrintDuplex(string PrintDuplexContent)
+    {
+        Console.WriteLine("Print Duplex content");
+    }
+}
+
+/// With Interface Segregation Principle
+public class HPLaserJetPrinter : IPrinterTasks, IFaxTasks, IPrintDuplexTasks
+{
+    public void Print(string PrintContent)
+    {
+        Console.WriteLine("Print Done");
+    }
+
+    public void Scan(string ScanContent)
+    {
+        Console.WriteLine("Scan content");
+    }
+
+    public void Fax(string FaxContent)
+    {
+        Console.WriteLine("Fax content");
+    }
+
+    public void PrintDuplex(string PrintDuplexContent)
+    {
+        Console.WriteLine("Print Duplex content");
+    }
+}
