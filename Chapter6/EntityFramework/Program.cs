595db4d810e28f04e48c0fbc249fd44a80e7e0e2
Novrian Fajar Hidayat <novrian@mic.ms>
2022-01-21 13:13:57 +0700

    orm-entityframework

---
+++ Chapter6/EntityFramework/Program.cs
@@ -0,0 +1,34 @@
+﻿// 
+using (var context = new MyContext())
+{
+    //INPUT DATA TO DB
+    var customerA = new Customer
+    {
+        CustomerId = 1,
+        FirstName = "Mamang",
+        LastName = "Daya",
+        Address = "Banten"
+    };
+
+    var customerB = new Customer 
+    {
+        CustomerId = 2,
+        FirstName = "Abah",
+        LastName = "Sayuti",
+        Address  = "Subang",
+    };
+
+    context.Customers.Add(customerA);
+    context.Customers.Add(customerB);
+
+    context.SaveChanges();
+
+
+    //RETRIEVE DATA FROM DB
+    var customers = context.Customers;
+
+    foreach(var customer  in customers)
+    {
+        Console.WriteLine($"{customer.FirstName} {customer.LastName} - {customer.Address}");
+    }
+}
