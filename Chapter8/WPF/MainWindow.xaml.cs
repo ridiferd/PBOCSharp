9c47c0f3615b5e278b5c8fd1f040a118aff30a82
Novrian Fajar Hidayat <novrian@mic.ms>
2022-01-21 14:55:47 +0700

    WPF

---
+++ Chapter8/WPF/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CustomerGrid.DataContext = GetCustomers();
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            var result = new List<Customer>();
+
+            result.Add(new Customer()
+            {
+                Firstname = "Alexander",
+                LastName = "Frostbite",
+                Address = "Sweden Street"
+            });
+
+            result.Add(new Customer()
+            {
+                Firstname = "Juliano",
+                LastName = "Maradondo",
+                Address = "Venezuela Street"
+            });
+
+            result.Add(new Customer()
+            {
+                Firstname = "Dominique",
+                LastName = "Elizabeth",
+                Address = "New Jersey Steet"
+            });
+
+            return result;
+        }
+    }
+}
---
+++ Chapter8/WPF/Models/Customer.cs
@@ -0,0 +1,7 @@
+public class Customer
+{
+    public string Firstname { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+}
+
