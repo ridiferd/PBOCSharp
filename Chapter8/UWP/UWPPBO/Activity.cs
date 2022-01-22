11c26653ee095a21a1d47d84311f7fa38fd80abe
Novrian Fajar Hidayat <novrian@mic.ms>
2022-01-22 16:29:40 +0700

    added UWP

---
+++ Chapter8/UWP/UWPPBO/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPPBO
+{
+    public class Activity 
+    {
+        public string activity { get; set; }
+        public string type { get; set; }
+        public int participants {get;set;}
+        public double price { get; set; }
+        public string link { get; set; }
+        public string key { get; set; }
+        public int accessability { get; set; }
+    }
+}
---
+++ Chapter8/UWP/UWPPBO/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPPBO
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public Activity SelectedActivity { get; set; } 
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.SelectedActivity = new Activity();
+            this.DataContext = SelectedActivity;
+        }
+
+        private void GetActivity()
+        {
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.BaseAddress = "https://www.boredapi.com/api/";
+                    var json = webClient.DownloadString("activity");
+                    Activity act = JsonConvert.DeserializeObject<Activity>(json);
+                    this.DataContext = act;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GetActivity();
+        }
+
+
+    }
+}
